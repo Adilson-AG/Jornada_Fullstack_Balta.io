25e5fdf34346e7eec3a45ad83fc50f9141a68921
Adilson Antonio Genari Filho <80858375+Adilson-AG@users.noreply.github.com>
2024-05-28 16:08:05 -0300

    Add project files.

---
+++ Fina.Core/Configuraton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fina.Core
+{
+   public static class Configuraton
+    {
+        public const int DefaultStatusCode = 200;
+        public const int DefaultPageNumber = 1;
+        public const int DefaultPageSize = 25;
+    }
+}
---
+++ Fina.Core/Requests/Transactions/CreateTransactionRequest.cs
@@ -0,0 +1,31 @@
+﻿using Fina.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fina.Core.Requests.Transactions
+{
+    public class CreateTransactionRequest : Request
+    {
+        [Required(ErrorMessage = "Título inváido")]
+        [MaxLength(80, ErrorMessage = "O título deve conter até 80 caracteres")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Tipo inváido")]
+        public ETransactionType Type { get; set; } = ETransactionType.Withdraw;
+
+        [Required(ErrorMessage = "Valor inválido")]
+        public decimal Amount { get; set; }
+
+        [Required(ErrorMessage = "Categoria inválida")]
+        public long CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Data inválida")]
+        public DateTime PaidOrReceivedAt { get; set; }
+
+        
+    }
+}
